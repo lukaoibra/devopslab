b5fb754dcf5b36c5ca41b781494d9b82ce7e9d72
lukaoibra <88867674+lukaoibra@users.noreply.github.com>
2022-05-18 23:27:48 -0300

    Bug Added

--- app.py
+++ app.py
@@ -21,3 +21,27 @@ def bad():
 
 if __name__ == '__main__':
     app.run()
+
+def foo():
+    try:
+        raise FloatingPointError()
+    except (ArithmeticError, RuntimeError) as e:
+        print(e)
+    except FloatingPointError as e: # Noncompliant. FloatingPointError is a subclass of ArithmeticError
+        print("Never executed")
+    except OverflowError as e: # Noncompliant. OverflowError is a subclass of ArithmeticError
+        print("Never executed")
+
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e: # Noncompliant. Duplicate Except.
+        print("Never executed")
+
+    try:
+        raise ValueError()
+    except BaseException as e:
+        print(e)
+    except: # Noncompliant. This is equivalent to "except BaseException" block
+        print("Never executed")
